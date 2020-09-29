62280819db7180d28cac5834ac1a6c41f15ed113
trevismcgill <trevismcgill25@gmail.com>
2020-09-29 16:04:51 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: true
+    with_options if: :is_released? do |song|
+        song.validates :release_year, presence: true, numericality: {less_than_or_equal_to: Date.today.year}
+    end
+ 
+
+    def is_released?
+        self.released
+    end
+end
