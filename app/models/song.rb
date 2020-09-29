36fcd1ad99ef3c68e0d0b85bc1643f8329136eba
NickSpangler <Spangleyman@mac.com>
2020-09-29 11:44:19 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ApplicationRecord
+    validates :title, presence: true, uniqueness: { scope: :release_year, message: "artist cannot repeat a title in the same year" }
+    validates :released, inclusion: { in: [true, false] }
+    validates :release_year, presence: true, if: :released?, numericality: { less_than_or_equal_to: Date.current.year }
+    validates :artist_name, presence: true
+end
+
