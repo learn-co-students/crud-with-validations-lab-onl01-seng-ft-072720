5f958de9f372845ba3742e2d95eead4620eabded
EvanRPavone <EvanRPavone@gmail.com>
2020-09-29 12:52:36 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "No Woman No Cry", released: true, release_year: 1978, artist_name: "Bob Marley", genre: "Reggae")
+
+Song.create(title: "In New York", released: true, release_year: 2009, artist_name: "Jay Z, Alicia Keys", genre: "Rap/R&B")
