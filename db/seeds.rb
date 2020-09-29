ae6fd6ea660d16ad5ab0d5065a69ef0adcbcd3b1
VirgilCantu <cantu.sampsom@gmail.com>
2020-09-28 20:30:17 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,6 @@
+Song.create(title: "Rock With You", released: true, release_year: 1989, artist_name: "Jackson 5", genre: "Pop" )
+Song.create(title: "Love Shack", released: true, release_year: 1985, artist_name: "B-52s", genre: "Rock" )
+Song.create(title: "Baby", released: true, release_year: 2010, artist_name: "Justin Beiber", genre: "Pop" )
+Song.create(title: "Walk the Line", released: true, release_year: 1978, artist_name: "Johhny Cash", genre: "Folk" )
+Song.create(title: "Have You Ever Seen The Rain?", released: true, release_year: 1999, artist_name: "Creedence Clearwater Revival", genre: "Rock" )
+Song.create(title: "Dreamin'", released: true, release_year: 2020, artist_name: "53 Thieves", genre: "Electronic" )
